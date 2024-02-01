1fe51344583eb37417fc4b083d92a1d7136352cb
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2024-02-01 12:10:34 +0200

    Designed Sidebar Component

--- app/(Home)/(routes)/layout.jsx
+++ app/(Home)/(routes)/layout.jsx
@@ -4,7 +4,7 @@ import SideBarNav from '../_components/SideBarNav'
 function HomeLayout({ children }) {
   return (
     <div>
-      <div>
+      <div className='h-full w-64 flex flex-col fixed inset-y-0 z-50'>
         <SideBarNav />
       </div>
       {children}
--- app/(Home)/_components/SideBarNav.jsx
+++ app/(Home)/_components/SideBarNav.jsx
@@ -1,12 +1,54 @@
+'use client'
+import { Layout, Mail, Search, Shield } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+
+const menuList = [
+  {
+    id: 1,
+    name: 'Browse',
+    icon: Search,
+    path: '/browse'
+  },
+  {
+    id: 2,
+    name: 'Dashboard',
+    icon: Layout,
+    path: '/dashboard'
+  },
+  {
+    id: 3,
+    name: 'Upgrade',
+    icon: Shield,
+    path: '/upgrade'
+  },
+  {
+    id: 4,
+    name: 'Newsletter',
+    icon: Mail,
+    path: '/newsletter'
+  },
+]
 
 function SideBarNav() {
+  const [activeIndex, setActiveIndex] = useState(0)
   return (
-    <div>
-      <Image 
-      
-      />
+    <div className='h-full bg-white border-r flex flex-col overflow-y-auto shadow-md'>
+      <div className='p-5 border-b'>
+        <Image 
+          src="/logo.svg"
+          width={170}
+          height={100}
+        />
+      </div>
+      <div className='flex flex-col'>
+        {menuList.map((item, index) => (
+          <div onClick={() => setActiveIndex(index)}  key={item.id} className={`flex gap-2 items-center px-6 py-4 cursor-pointer text-gray-500 hover:bg-gray-100 ${activeIndex == index ? "bg-purple-50 text-purple-800" : null}`}>
+            <item.icon />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
